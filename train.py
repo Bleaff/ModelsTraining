c0844ebadb7699036c42189d7dda93b5278729b3
Sysoev Sergei <sysoev-sv@mail.com>
2024-03-30 16:18:35 +0300

    experiments

--- train.py
+++ train.py
@@ -5,9 +5,9 @@ model = YOLO('/home/bleaf/train_people/yolo_model/yolov8m.pt')
  
 # Training.
 results = model.train(
-   data='/home/bleaf/train_people/merged_dataset_people/meta.yaml',
+   data='/home/bleaf/train_people/filtered_dataset/meta.yaml',
    imgsz=640,
-   epochs=50,
+   epochs=200,
    batch=16,
-   name='yolov8n_v8_50e'
+   name='yolov8m_200e'
 )
